693d62929fac664bf93b840cdff9a4fbfedde834
Akmaral Abilkhaiyr <250465@astanait.edu.kz>
2026-05-16 15:32:48 +0500

    Update Library management logic

--- library.py
+++ library.py
@@ -1,24 +1,38 @@
 from file_manager import load_books, save_books
 
+
+def log_action(func):
+    def wrapper(*args, **kwargs):
+        print("Library action started")
+        result = func(*args, **kwargs)
+        print("Library action finished")
+        return result
+
+    return wrapper
+
+
 class Library:
     def __init__(self):
         self.books = load_books()
 
+    @log_action
     def add_book(self, book):
         self.books.append(book)
 
     def delete_book(self, index):
-        self.books[index]
+        del self.books[index]
 
     def borrow_book(self, index):
         if self.books[index].status == "Available":
             self.books[index].status = "Borrowed"
 
     def return_book(self, index):
-        self.books[index].status = "Returned"
+        self.books[index].status = "Available"
+
+    def available_books_generator(self):
+        for book in self.books:
+            if book.status == "Available":
+                yield book
 
     def save_data(self):
         save_books(self.books)
-
-
-
