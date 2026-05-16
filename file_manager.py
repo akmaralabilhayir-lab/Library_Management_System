2c6ee7c9d3da9224418dba48fbffc8319841d789
Akmaral Abilkhaiyr <250465@astanait.edu.kz>
2026-05-16 14:46:18 +0500

    Add JSON file manager

---
+++ file_manager.py
@@ -0,0 +1,30 @@
+import json
+from book import Book
+
+FILE_NAME = "book.json"
+
+def load_books():
+    try:
+        with open(FILE_NAME, "r") as file:
+            data = json.load(file)
+            books = []
+            for item in data:
+                book = Book(
+                    item["title"],
+                    item["author"],
+                    item["year"],
+                    item["status"]
+                )
+
+                books.append(book)
+                return books
+    except FileNotFoundError:
+        return []
+
+def save_books(books):
+    data = []
+    for book in books:
+        data.append(book.to_dict())
+
+    with open(FILE_NAME, "w") as file:
+        json.dump(data, file, indent=4)
